b5edc0a2af4f5d9f82c40d45fbc59372bd55d88c
Elena Maltseva <lena.3275@ya.ru>
2021-01-19 23:27:55 +0300

    feat(table): add markup & styles for resize element in rows and columns

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,13 +3,31 @@ const CHAR_CODES = {
   Z: 90,
 }
 
+export function createTable(rowsCount = 10) {
+  const rows = []
+
+  const colsCount = CHAR_CODES.Z - CHAR_CODES.A + 1
+  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
+
+  rows.push(createRow(cols))
+
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount).fill('').map(toCell).join('')
+    rows.push(createRow(cells, i + 1))
+  }
+
+  return rows.join('')
+}
+
 function toChar(_, index) {
   return String.fromCharCode(CHAR_CODES.A + index)
 }
 
-
 function toColumn(content) {
-  return `<div class="column">${content}</div>`
+  return `<div class="column">
+    ${content}
+    <div class='column__resize'></div>
+  </div>`
 }
 
 function toCell(content) {
@@ -17,33 +35,14 @@ function toCell(content) {
 }
 
 function createRow(content, index = '') {
+  const resizer = index ? `<div class='row__resize'></div>` : ''
   return `
     <div class='row'>
-      <div class="row__info">${index}</div>
+      <div class="row__info">
+        ${index}
+        ${resizer}
+      </div>
       <div class="row__data">${content}</div>
     </div>
   `
-}
-
-export function createTable(rowsCount = 10) {
-  const rows = []
-
-  const colsCount = CHAR_CODES.Z - CHAR_CODES.A + 1
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('')
-
-  rows.push(createRow(cols))
-
-  for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount)
-        .fill('')
-        .map(toCell)
-        .join('')
-    rows.push(createRow(cells, i + 1))
-  }
-
-  return rows.join('')
 }
