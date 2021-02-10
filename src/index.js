9ac15dcf478b303d1269912c35f315892374eb10
Elena Maltseva <lena.3275@ya.ru>
2021-02-10 23:02:44 +0300

    feat: add initial state

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import { Excel, Header, Toolbar, Formula, Table } from '~/components'
 import { createStore } from '~core/createStore'
 import { rootReducer } from '~/redux/rootReducer'
 import { storage } from '~core/utils'
+import { initialState } from '~/redux/initialState'
 
-const store = createStore(rootReducer, storage('excel-state'))
+const store = createStore(rootReducer, initialState)
 store.subscribe(state => {
   storage('excel-state', state)
 })
