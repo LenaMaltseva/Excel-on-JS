dc84c79bc65d8d657951e3877bcef2b89072e976
Elena Maltseva <lena.3275@ya.ru>
2021-05-05 12:40:08 +0300

    feat: connect app with router

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
+import { Router } from '~core/routes/Router'
+import { DashboardPage, ExcelPage } from '~/pages'
 import './styles/index.scss'
-import { Excel, Header, Toolbar, Formula, Table } from '~/components'
-import { createStore } from '~core/createStore'
-import { rootReducer } from '~/redux/rootReducer'
-import { storage, debounce } from '~core/utils'
-import { initialState } from '~/redux/initialState'
 
-const store = createStore(rootReducer, initialState)
 
-const stateListener = debounce(state => {
-  storage('excel-state', state)
-}, 300)
-
-store.subscribe(stateListener)
-
-const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table],
-  store,
+new Router('#app', {
+  dashboard: DashboardPage,
+  excel: ExcelPage,
 })
-
-excel.render()
