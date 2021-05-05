606658297ab3d140fa84d74fb214e121e2ed961c
Elena Maltseva <lena.3275@ya.ru>
2021-05-05 12:42:18 +0300

    feat: handle actions in table's header, add titles to toolbar's buttons, edit dashboard styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,9 @@
 import { ExcelComponent } from '~core/ExcelComponent'
 import * as actions from '~/redux/actions'
+import { $ } from '~core/Dom'
 import { debounce } from '~core/utils'
 import { defaultTitle } from '~/constants'
+import { ActiveRoute } from '~core/routes/ActiveRoute'
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -21,21 +23,36 @@ export class Header extends ExcelComponent {
   toHTML() {
     const title = this.store.getState().tableTitle || defaultTitle
     return `
-      <input id="table-title" type="text" value="${title}"/>
+      <input id="table-title" data-item="title" type="text" value="${title}"/>
       <div>
-        <div class="button">
-          <i class="material-icons">delete_outline</i>
+        <div class="button" title="Delete spreadsheet" data-item="delete">
+          <i class="material-icons" data-button="delete">delete_outline</i>
         </div>
 
-        <div class="button">
-          <i class="material-icons">exit_to_app</i>
+        <div class="button" title="Back to dashboard" data-item="exit">
+          <i class="material-icons" data-button="exit">exit_to_app</i>
         </div>
       </div>
     `
   }
 
-  onClick() {
-    this.$emit('tableTitle:input')
+  onClick(e) {
+    const $target = $(e.target)
+
+    switch ($target.data.item) {
+      case 'delete':
+        if (confirm('Are you sure you want to delete this spreadsheet?')) {
+          localStorage.removeItem(`excel:${ActiveRoute.param}`)
+          ActiveRoute.navigate('')
+        }
+        break
+      case 'exit':
+        ActiveRoute.navigate('')
+        break
+      case 'input':
+      default:
+        this.$emit('tableTitle:input')
+    }
   }
 
   onInput(e) {
