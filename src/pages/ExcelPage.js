51226328627ae45dffd8433eeb7fb8a3adf1a813
Elena Maltseva <lena.3275@ya.ru>
2021-05-05 12:39:04 +0300

    feat: add pages

---
+++ src/pages/ExcelPage.js
@@ -0,0 +1,35 @@
+import { Page } from '~core/Page'
+import { Excel, Formula, Header, Table, Toolbar } from '~/components'
+import { debounce, storage } from '~core/utils'
+import { createStore } from '~core/createStore'
+import { rootReducer, initialState } from '~/redux'
+
+export class ExcelPage extends Page {
+  getRoot() {
+    const excelKey =
+      `excel:${this.params ? this.params : Date.now().toString()}`
+
+    const store = createStore(rootReducer, storage(excelKey) || initialState)
+
+    const stateListener = debounce(state => {
+      storage(excelKey, state)
+    }, 300)
+
+    store.subscribe(stateListener)
+
+    this.excel = new Excel({
+      components: [Header, Toolbar, Formula, Table],
+      store,
+    })
+
+    return this.excel.getRoot()
+  }
+
+  afterRender() {
+    this.excel.init()
+  }
+
+  destroy() {
+    this.excel.destroy()
+  }
+}
